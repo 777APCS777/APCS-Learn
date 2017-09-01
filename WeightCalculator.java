a7ec500e3b1f248eda6ea875eed3f0ac5df6ac5d
Ethan <ethanchausb@gmail.com>
2017-09-01 11:30:00 -0700

    Updated file

---
+++ WeightCalculator.java
@@ -0,0 +1,25 @@
+public class WeightCalculator
+{
+public static void main(String[] args)
+{
+
+//EarthWeight
+double E = Double.parseDouble(args[0]);
+System.out.println("Mars Weight "+E*.38);
+System.out.println("Jupiter Weight "+E*2.36);
+System.out.println("Venus Weight "+ E*.91);
+System.out.println("Saturn Weight "+E*1.06);
+System.out.println("Neutron Star "+E*140000000000);
+
+}
+
+
+
+ 
+
+}
+
+
+
+
+
